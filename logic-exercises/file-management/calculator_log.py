9c8c8bcef5bfcacdb91ee4b8878ca465592c2c4c
Dario Perez <darioperez.2105@gmail.com>
2026-02-15 00:30:33 -0300

    fix: handle empty or corrupted JSON files using JSONDecodeError

---
+++ logic-exercises/file-management/calculator_log.py
@@ -0,0 +1,22 @@
+import json
+
+
+def log_calculation(num1, num2, result, file_name="operation.json"):
+    try:
+        try:
+            with open(file_name, "r") as file:
+                history = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            history = []
+
+        history.append({"Number 1": num1, "Number 2": num2, "Result": result})
+
+        with open(file_name, "w") as file:
+            json.dump(history, file, indent=4)
+        print("✅ Data calculation run correctly.")
+
+    except Exception as e:
+        print(f"❌ Failed to save log: {e}")
+
+
+log_calculation(5, 10, 15)
