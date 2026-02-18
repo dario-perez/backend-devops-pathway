0149e4e4ed88e1bcc73c2b964c12abcb4d31e89f
Dario Perez <darioperez.2105@gmail.com>
2026-02-18 18:41:14 -0300

    feat: complete security gatekeeper with path handling and error management

---
+++ logic-exercises/week-1/security-gatekeeper/gatekeeper.py
@@ -0,0 +1,33 @@
+import json
+import os
+
+script_dir = os.path.dirname(__file__)
+
+access_file = os.path.join(script_dir, "access_log.json")
+
+try:
+    with open(access_file, "r") as file:
+        access = json.load(file)
+
+    successes = 0
+    failures = 0
+    suspect_users = []
+
+    for entry in access:
+        if entry["success"]:
+            successes += 1
+        else:
+            failures += 1
+            if entry["user"] not in suspect_users:
+                suspect_users.append(entry["user"])
+
+    print("📊 --- SECURITY REPORT ---")
+    print(f"✅ Success cases: {successes}")
+    print(f"❌ Failure cases: {failures}")
+    print(f"👤 Suspect users: {', '.join(suspect_users) if suspect_users else 'None'}")
+    print("--------------------------")
+
+except FileNotFoundError:
+    print("⚠️ Error: Access log not found. Please check the source.")
+except json.JSONDecodeError:
+    print("⚠️ Error: The log file exists but it's corrupted or empty.")
--- logic-exercises/week-1/test.py
+++ logic-exercises/week-1/test.py
@@ -50,3 +50,82 @@
 #         max_num = num
 
 # print(max_num)
+
+
+# Password generator_______________________________________________________
+# import random
+
+# LETTERS = [
+#     "a",
+#     "b",
+#     "c",
+#     "d",
+#     "e",
+#     "f",
+#     "g",
+#     "h",
+#     "i",
+#     "j",
+#     "k",
+#     "l",
+#     "m",
+#     "n",
+#     "o",
+#     "p",
+#     "q",
+#     "r",
+#     "s",
+#     "t",
+#     "u",
+#     "v",
+#     "w",
+#     "x",
+#     "y",
+#     "z",
+#     "A",
+#     "B",
+#     "C",
+#     "D",
+#     "E",
+#     "F",
+#     "G",
+#     "H",
+#     "I",
+#     "J",
+#     "K",
+#     "L",
+#     "M",
+#     "N",
+#     "O",
+#     "P",
+#     "Q",
+#     "R",
+#     "S",
+#     "T",
+#     "U",
+#     "V",
+#     "W",
+#     "X",
+#     "Y",
+#     "Z",
+# ]
+
+# NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+# SYMBOLS = ["#", "$", "%", "&", "@", "!", "?", "*", "+"]
+
+# password = []
+
+# print("welcome to the PyPassword Generator!")
+# n_letters = int(input("How many letters would you like?\n"))
+# n_numbers = int(input("How many numbers would you like?\n"))
+# n_symbols = int(input("How many symbols would you like?\n"))
+
+# password.append(random.choices(LETTERS, k=n_letters))
+# password.append(random.choices(NUMBERS, k=n_numbers))
+# password.append(random.choices(SYMBOLS, k=n_symbols))
+
+# flat_password = password[0] + password[1] + password[2]
+# random.shuffle(flat_password)
+
+# print(f"{''.join(flat_password)}")
