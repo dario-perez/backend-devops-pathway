6acf96e7a65faddf539ec6673cbe2a3c87ce837f
Dario Perez <darioperez.2105@gmail.com>
2026-02-18 02:13:29 -0300

    chore: major repository reorganization for scalability and OOP phase

---
+++ logic-exercises/week-1/test.py
@@ -0,0 +1,52 @@
+# Heads and tails_______________________________________________________
+# import random
+
+
+# def flip_a_coin():
+#     return "Heads" if random.randint(0, 1) == 1 else "Tails"
+
+
+# print(flip_a_coin())
+
+
+# Who pays the bill_______________________________________________________
+# import random
+
+# friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+# # option1 (with index)
+# print(f"who pays the bill is: {friends[random.randrange(len(friends))]}!")
+# # option2 (without index)
+# print(f"Who pays the bill is: {random.choice(friends)}")
+
+
+# Rock, paper, scissors_______________________________________________________
+# nums = [
+#     142,
+#     27,
+#     89,
+#     115,
+#     12,
+#     56,
+#     134,
+#     43,
+#     98,
+#     71,
+#     29,
+#     108,
+#     41,
+#     74,
+#     125,
+#     17,
+#     79,
+#     113,
+#     58,
+# ]
+
+# max_num = 0
+
+# for num in nums:
+#     if num > max_num:
+#         max_num = num
+
+# print(max_num)
