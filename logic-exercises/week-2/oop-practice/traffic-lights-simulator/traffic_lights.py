03f9fe1c9ad56b7e6a055b91d81ac5d953d826ca
Dario Perez <darioperez.2105@gmail.com>
2026-03-07 17:13:22 -0300

    feat: implement animated traffic light simulator with state management

---
+++ logic-exercises/week-2/oop-practice/traffic-lights-simulator/traffic_lights.py
@@ -0,0 +1,39 @@
+import time
+
+
+class TrafficLights:
+  def __init__(self, color: str = "red"):
+    self.color = color
+
+  def change(self):
+    if self.color == "red":
+      self.color = "green"
+    elif self.color == "green":
+      self.color = "yellow"
+    else:
+      self.color = "red"
+
+  def show(self):
+    print(self)
+  
+  def __str__(self):
+    mapping = {
+            "red": "🔴 STOP",
+            "green": "🟢 GO",
+            "yellow": "🟡 CAUTION"
+        }
+    return mapping.get(self.color)
+
+
+
+if __name__ == "__main__":
+  traffic_light = TrafficLights()
+  traffic_light.change()
+  traffic_light.show()
+
+  print("--- 🚦 Simulator Starting ---")
+  for i in range(10):
+    print("\033[H\033[J", end="")
+    traffic_light.show()
+    traffic_light.change()
+    time.sleep(1)
