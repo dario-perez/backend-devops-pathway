768b690bad8099c67aada80f205b7a3fea2a4e4d
Dario Perez <darioperez.2105@gmail.com>
2026-05-05 23:22:30 -0300

    fix: Automation scripts updated

---
+++ logic-exercises/python/week-2/oop-practice/16-inventory-logic/inventory_logic.py
@@ -0,0 +1,6 @@
+ventas = [
+    {"producto": "monitor", "precio": 100, "cantidad": 2},
+    {"producto": "teclado", "precio": 50, "cantidad": 1},
+    {"producto": "monitor", "precio": 100, "cantidad": 1}
+]
+
